b17d0341520501fe50e11b27b8ddbffe0ad7c464
DoctorDoge <aloysiuslee97@hotmail.com>
2022-03-03 21:02:16 +0800

    Updated error handling

--- indeed.py
+++ indeed.py
@@ -3,39 +3,56 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+# Get URL from user input
 def getURL(job, location):
-  urlString = "https://sg.indeed.com/jobs?q={}&l={}"
-  url = urlString.format(job, location)
-  return url
+    urlString = "https://sg.indeed.com/jobs?q={}&l={}"
+    url = urlString.format(job, location)
+    return url
 
+# Get individual job entry
 def getJobEntry(jobTitleDiv, soup, num):
-  # Check if "new" is in span
-  try:
-    jobTitle = jobTitleDiv[num].h2.find_all('span')[1].text
-  except IndexError:
-    jobTitle = jobTitleDiv[num].h2.find_all('span')[0].text
+    # Check if "new" is in span
+    try:
+        try:
+            jobTitle = jobTitleDiv[num].h2.find_all('span')[1].text
+        except IndexError:
+            jobTitle = jobTitleDiv[num].h2.find_all('span')[0].text
+    except AttributeError:
+        jobTitle = ""
 
-  jobCompany = soup.find_all("span", "companyName")[num].text
+    try:
+        jobCompany = soup.find_all("span", "companyName")[num].text
+    except AttributeError:
+        jobCompany = ""
 
-  # Check if "+ location" is in span
-  jobLocation = soup.find_all("div", "companyLocation")[num].text
-  jobLocation = re.sub('\+.*','',jobLocation)
+    try:
+        # Check if "+ location" is in span
+        jobLocation = soup.find_all("div", "companyLocation")[num].text
+        jobLocation = re.sub('\+.*','',jobLocation)
+    except AttributeError:
+        jobLocation = ""
 
-  jobDescription = soup.find_all("div", "job-snippet")[num].text.strip()
-  jobEntry = (jobTitle, jobCompany, jobLocation, jobDescription)
-  
-  return jobEntry
+    try:
+        jobDescription = soup.find_all("div", "job-snippet")[num].text.strip()
+    except AttributeError:
+        jobDescription = ""
+
+    jobEntry = (jobTitle, jobCompany, jobLocation, jobDescription)
+    
+    return jobEntry
 
 def main(job, location):
-  url = getURL(job, location)
+    url = getURL(job, location)
 
-  response = requests.get(url)
-  soup = BeautifulSoup(response.text, "html.parser")
+    # Parse URL into BeautifulSoup
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-  jobTitleDiv = soup.find_all("div", "heading4")
-  
-  for x in range(len(jobTitleDiv)):
-    jobEntry = getJobEntry(jobTitleDiv, soup, x)
-    print(jobEntry)
+    jobTitleDiv = soup.find_all("div", "heading4")
+    
+    # Parse all job entries on page
+    for x in range(len(jobTitleDiv)):
+        jobEntry = getJobEntry(jobTitleDiv, soup, x)
+        print(jobEntry)
 
 main("retail", "singapore")
